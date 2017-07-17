7dd33a0606c2fad4dcf760ab1361f6bf97a4b51c
syed_tanveer <syedtanveerabid@live.com>
2017-07-17 12:06:13 +0530

    Add solution for problem code EIGHTS

---
+++ EIGHTS.cpp
@@ -0,0 +1,46 @@
+/***************************
+*  -author-  syed_tanveer
+*  -Problem- EIGHTS
+****************************/
+ 
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <utility> /// pair
+#include <vector>
+#include <map>
+#include <set>
+#include <bitset>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+ 
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define l s.length()-1
+using namespace std;
+typedef long long ll;
+typedef vector <int> vint;
+typedef vector <vint> vvint;
+typedef vector <string> vstring;
+typedef vector <ll> vll;
+ 
+ 
+int main()
+{
+    int t;
+    ll a;
+    cin>>t;
+    while(t--){
+        cin>>a;
+        cout<<250*(a-1)+192<<endl;
+    }
+    return 0;
+}
